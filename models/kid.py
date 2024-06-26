e54a722a0323227c3997b743f0bcd12587bcfb1e
Gilmar Neves <gilmarneves@outlook.com>
2024-06-26 20:39:41 -0300

    Non mandatory field fix

--- models/kid.py
+++ models/kid.py
@@ -7,7 +7,7 @@ class KidModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     sex = db.Column(db.String(10), unique=False, nullable=False)
-    parents = db.Column(db.String(100), unique=True, nullable=False)
+    parents = db.Column(db.String(100), unique=False, nullable=False)
     birthdate = db.Column(db.Date, unique=False, nullable=False)
 
     items = db.relationship(
